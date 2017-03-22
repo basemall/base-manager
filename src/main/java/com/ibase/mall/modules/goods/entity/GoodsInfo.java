de5c8d715c4142377ada66bb3f019cc10b61416e
apollo2099 <liaoxingjian.2008@163.com>
2017-03-22 23:23:19 +0800

    feat(base-manager): 新增个性化包名兼容配置和测试demo

--- src/main/java/com/ibase/mall/modules/goods/entity/GoodsInfo.java
+++ src/main/java/com/ibase/mall/modules/goods/entity/GoodsInfo.java
@@ -1,7 +1,28 @@
 package com.ibase.mall.modules.goods.entity;
 
+import java.math.BigDecimal;
+
 import com.thinkgem.jeesite.common.persistence.DataEntity;
 
 public class GoodsInfo extends DataEntity<GoodsInfo>{
+
+	private static final long serialVersionUID = 7720023756084746523L;
+	private String goodsName;
+	private BigDecimal goodsPrice;
+	
+	public String getGoodsName() {
+		return goodsName;
+	}
+	public void setGoodsName(String goodsName) {
+		this.goodsName = goodsName;
+	}
+	public BigDecimal getGoodsPrice() {
+		return goodsPrice;
+	}
+	public void setGoodsPrice(BigDecimal goodsPrice) {
+		this.goodsPrice = goodsPrice;
+	}
+	
+	
 
 }
